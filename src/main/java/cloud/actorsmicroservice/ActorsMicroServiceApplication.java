141eab9e578c5077eba820abaa81436852c9f03f
DeanAvram <dean6198@gmail.com>
2024-06-27 12:38:49 +0300

    First commit

---
+++ src/main/java/cloud/actorsmicroservice/ActorsMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.actorsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorsMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/actorsmicroservice/ActorsMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.actorsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorsMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
